451cfaab04bfb7f1050de590938d4a4c1caafe88
Lizu-4 <lizugas2002@gmail.com>
2022-11-22 04:52:29 +0000

    trabajo listo

---
+++ app.js
@@ -0,0 +1,13 @@
+let pronouns = ['the','our'];
+let adjs = ['great', 'big' ];
+let nouns = ['jogger','racoon'];
+
+for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+        for (const noun of nouns) {
+            console.log(pronoun+adj+noun+".com");
+        }
+    }
+}
+
+//sorry no lei bien las indicaciones :( (deberian tener un espacio para comentar la entrega de proyectos como en google classroom jej)
